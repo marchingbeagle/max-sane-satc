c9c44e647636441ba7accf6801464f19522cb988
GabrielWDuarte <145812580+GabrielWDuarte@users.noreply.github.com>
2024-04-11 19:14:32 -0300

    DML - dummy data

---
+++ sql/DML.MAX.sql
@@ -0,0 +1,12 @@
+
+INSERT INTO mydb."User" ("User", "Role", "Password") VALUES
+('Eduarda Santos', 'Gerente de Vendas', 'senhasegura1'),
+('Marcos Silva', 'Representante de Vendas', 'senhasegura2'),
+('Larissa Oliveira', 'Comprador', 'senhasegura3');
+
+
+INSERT INTO mydb.Product ("Product", "Imagem", "Category") VALUES
+('Detergente', 'detergente.jpg', 'Limpeza'),
+('Desinfetante', 'desinfetante.jpg', 'Higiene'),
+('Sabão em Pó', 'sabao_em_po.jpg', 'Limpeza');
+
