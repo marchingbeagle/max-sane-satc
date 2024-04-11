7ffbc894d39b95ac5dbbd9abe3a18bf94d5ae6b4
Erik Schneider <erik.schneider.pacheco@gmail.com>
2024-04-11 19:47:38 -0300

    initial files for the backend

---
+++ back/src/main/java/maxsane/maxsanewebsite/MaxSaneWebsiteApplication.java
@@ -0,0 +1,13 @@
+package maxsane.maxsanewebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaxSaneWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaxSaneWebsiteApplication.class, args);
+	}
+
+}
