b7106c0d9af98ad4f8ab69568bc5b823f66d4275
yndicaro <icroliveira3266@gmail.com>
2024-05-10 18:36:36 -0300

    Readme Atualizado

---
+++ front/app/crudteste/Crud/page.tsx
@@ -0,0 +1,17 @@
+import { getAllTodos } from "@/api";
+import AddTask from "../Components/AddTask";
+import TodoList from "../Components/TodoList";
+
+export default async function Home() {
+  const tasks = await getAllTodos();
+  console.log(tasks);
+  return (
+    <main className="className='max-w-4xl mx-auto mt-4'">
+      <div className="text-center my-5 flex flex-col gap-4">
+        <h1 className="text-3xl font-bold underline">Todo List App</h1>
+        <AddTask />
+      </div>
+      <TodoList tasks={tasks} />
+    </main>
+  );
+}
