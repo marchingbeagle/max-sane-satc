7fef1bb716df8107c42c5cc3c74c23b6ddbd29c1
Erik Schneider <erik.schneider.pacheco@gmail.com>
2024-06-04 19:21:27 -0300

    n aguento mais

--- back/src/main/java/maxsane/maxsanewebsite/services/EnvLoader.java
+++ back/src/main/java/maxsane/maxsanewebsite/services/EnvLoader.java
@@ -5,9 +5,7 @@ import io.github.cdimascio.dotenv.DotenvException;
 public class EnvLoader {
     public static void load(String path) {
         try {
-            Dotenv dotenv = Dotenv.configure()
-                    .directory(path) // Set the directory where .env is located
-                    .load();
+            Dotenv dotenv = Dotenv.configure().directory(path).load();
             String dbUrl = dotenv.get("DB_URL");
             String dbUsername = dotenv.get("DB_USERNAME");
             String dbPassword = dotenv.get("DB_PASSWORD");
