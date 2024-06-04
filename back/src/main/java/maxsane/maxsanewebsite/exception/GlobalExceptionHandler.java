8da4bf9231603e6ebe2b14e6b68a0d2b8024db20
Erik Schneider <erik.schneider.pacheco@gmail.com>
2024-06-04 18:50:57 -0300

    exception handler fix

--- back/src/main/java/maxsane/maxsanewebsite/exception/GlobalExceptionHandler.java
+++ back/src/main/java/maxsane/maxsanewebsite/exception/GlobalExceptionHandler.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package maxsane.maxsanewebsite.exception;
 
 import org.springframework.http.HttpStatus;
@@ -25,31 +24,3 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
-=======
-package maxsane.maxsanewebsite.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
-
-import java.util.HashMap;
-import java.util.Map;
-
-@ControllerAdvice
-public class GlobalExceptionHandler {
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleValidationExceptions(
-            MethodArgumentNotValidException ex, WebRequest request) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach(error -> {
-            String fieldName = ((org.springframework.validation.FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
-    }
-}
->>>>>>> 1708c9ae5543a942ae8dfc8ba6b1d7e4f0481ded
